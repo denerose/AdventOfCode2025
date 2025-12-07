8d2b099bf91d49d8b05406ff23152d70ac97c14a
Saphron Hastie <109723086+denerose@users.noreply.github.com>
2025-12-07 12:10:54 +1100

    Prep Day4

--- app.ts
+++ app.ts
@@ -1,13 +1,17 @@
-// import { part1, part2 } from "./puzzles/day1.js";
+// import { part1 as part1day1, part2 as part2day1 } from "./puzzles/day1.js";
 // import { part1 as part1day2, part2 as part2day2 } from "./puzzles/day2.js";
-import { part1 as part1day3, part2 as part2day3 } from "./puzzles/day3.ts";
+// import { part1 as part1day3, part2 as part2day3 } from "./puzzles/day3.ts";
+import { part1 as part1day4, part2 as part2day4 } from "./puzzles/day4.ts";
 
 
-// console.log("Day 1, Part 1:", part1());
-// console.log("Day 1, Part 2:", part2());
+// console.log("Day 1, Part 1:", part1day1());
+// console.log("Day 1, Part 2:", part2day1());
 
 // console.log("Day 2, Part 1:", part1day2());
 // console.log("Day 2, Part 2:", part2day2());
 
-console.log("Day 3, Part 1:", part1day3());
-console.log("Day 3, Part 2:", part2day3());
+// console.log("Day 3, Part 1:", part1day3());
+// console.log("Day 3, Part 2:", part2day3());
+
+console.log("Day 4, Part 1:", part1day4());
+console.log("Day 4, Part 2:", part2day4());
---
+++ puzzles/day4.ts
@@ -0,0 +1,12 @@
+import { importFile } from "../utils/FileImporter.ts";
+
+const input = importFile("./inputs/day4.txt");
+const sample = importFile("./inputs/sample4.txt");
+
+export function part1(): number {
+  return 0;
+}
+
+export function part2(): number {
+  return 0;
+} 
